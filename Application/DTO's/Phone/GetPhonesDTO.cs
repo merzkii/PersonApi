2ad32564957846320924de2d6f4fa70ace869096
Merabi Chlaidze <chlaidze10@gmail.com>
2025-03-17 00:09:14 +0400

    change project names

---
+++ Application/DTO's/Phone/GetPhonesDTO.cs
@@ -0,0 +1,15 @@
+﻿using Application.DTO_s;
+using Application.DTO_s.Phone;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person.Application.DTO_s.Phone
+{
+    public class GetPhonesDTO:PhoneDTO
+    {
+        public PersonDTO Person { get; set; }
+    }
+}
